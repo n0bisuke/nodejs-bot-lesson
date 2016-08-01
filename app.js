cac6b90193686c9db8dc6674eb7d73f31472aefe
のびすけ <rsksound@gmail.com>
2016-08-02 03:25:12 +0900

    StreamingAPIでリプライを拾う

--- app.js
+++ app.js
@@ -3,17 +3,23 @@
 const Twitter = require('twitter');
 const client = new Twitter(require('./config'));
  
-let params = {screen_name: 'n0bisuke'};
-client.get('statuses/user_timeline', params, (error, tweets, response) => {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+// let params = {screen_name: 'n0bisuke'};
+// client.get('statuses/user_timeline', params, (error, tweets, response) => {
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
 
-let tweet = 'ねむ';
-client.post('statuses/update', {status : tweet}, (error, tweet, response) => {
-    if (error) {
-        process.stderr.write(error + '\n');
-        return;
-    }
+// let tweet = 'ねむ';
+// client.post('statuses/update', {status : tweet}, (error, tweet, response) => {
+//     if (error) {
+//         process.stderr.write(error + '\n');
+//         return;
+//     }
+// });
+
+client.stream('statuses/filter', {'track':'@n0bisuke'}, (stream) => {
+  stream.on('data', (data) => {
+    console.log(data);
+  });
 });
