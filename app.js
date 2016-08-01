633b2a606d56b8226873f7820493370942e00d17
のびすけ <rsksound@gmail.com>
2016-08-02 02:42:36 +0900

    鍵情報を外部ファイル化

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
+var client = new Twitter(require('./config'));
  
 var params = {screen_name: 'n0bisuke'};
 client.get('statuses/user_timeline', params, function(error, tweets, response){
